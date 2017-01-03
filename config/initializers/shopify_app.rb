9b9fa46b5b50b5970b33686ce09b85ea3c9b5d04
Cameron Anderson <andersc2@oregonstate.edu>
2017-01-03 15:31:05 -0800

    'generated-shopify-app'

---
+++ config/initializers/shopify_app.rb
@@ -0,0 +1,6 @@
+ShopifyApp.configure do |config|
+  config.api_key = "ae7106e2d366c7dc928842803e02fde1"
+  config.secret = "a9b3a28e494964c9242e89424d346aa7"
+  config.scope = "read_orders, read_products"
+  config.embedded_app = true
+end
